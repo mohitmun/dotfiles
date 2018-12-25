e29a38d7039a1ad2383b29e3538922b2381240bf
Mohit Munjani <mohitcrox@gmail.com>
2018-12-25 21:37:52 +0100

    duplicaate files

--- scripts/aliases.zsh
+++ scripts/aliases.zsh
@@ -201,6 +201,14 @@ gitcheckoutfilefrombranch(){
 alias wcl='wc -l'
 #alias ping='prettyping --nolegend'
 
+#========== Common ops ==============
+#Find duplicate files https://www.reddit.com/r/tinycode/comments/2ek3mi/fast_duplicate_file_finder_in_100_lines_of_c/ck0m716/
+find_duplicate_files(){
+  find $1 -type f -name '*' -print0 | xargs -0 md5sum > hashes.txt
+  sort -k 1,1 < hashes.txt > hashes-sorted.txt
+  uniq --check-chars=32 --all-repeated=separate hashes-sorted.txt
+}
+#========== Common ops ==============
 
 
 #========== System info =============
--- useful_commands.md
+++ useful_commands.md
@@ -1,11 +1,3 @@
-
-Find duplicate files https://www.reddit.com/r/tinycode/comments/2ek3mi/fast_duplicate_file_finder_in_100_lines_of_c/ck0m716/
-```
-#!/bin/sh
-find $1 -type f -name '*' -print0 | xargs -0 md5sum > hashes.txt
-sort -k 1,1 < hashes.txt > hashes-sorted.txt
-uniq --check-chars=32 --all-repeated=separate hashes-sorted.txt
-```
 pup is like jq but for html, pup is insane because traversing up using selector only wasnt possible (AFAIK). pup solves this problem. no nokogiri or bs4. pup FTW 
 ```
 pup ':parent-of(:parent-of(SELECTOR)) > a json' 
