900a9c4f2c37f2d0f3b4391019907be136a2ab77
Mohit Munjani <mohitcrox@gmail.com>
2020-04-01 12:26:08 +0530

    use async across zsh

--- scripts/prompt.zsh
+++ scripts/prompt.zsh
@@ -9,7 +9,7 @@ NEWLINE=$'\n'
 PROMPT_CHAR='❯'
 STATUS_BAR='$FG[241]$(get_battery)$DOT$(get_volume_indicator)$(get_todo_status)%{$reset_color%}'
 #autoload -Uz async && async
-source ~/.zsh-async/async.plugin.zsh
+#source ~/.zsh-async/async.plugin.zsh
 autoload -Uz vcs_info
 
 jobs_prompt() {
